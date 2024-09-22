6f7ad982a8af6a36d2bf80db655f93c2c968d22d
amr <110623837+amrahmed161@users.noreply.github.com>
2024-09-23 01:13:03 +0300

    create dashboard and create view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,32 @@
+@extends('admin.master')
+
+
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+   <section>
+    <div class="card col-8 offset-2">
+      <div class="card-header">Welcome my dashboard</div>
+      <div class="card-body">
+        h1 we are Amr AHMED 
+      </div>
+    </div>
+   </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard',function(){
+    return view('admin.dashboard');
+});
+Route::get('/users',function(){
+    return view('admin.users');
+});
